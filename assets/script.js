f02514db3c9a0d0bbf564d003db3ee7de7da8312
long bin <david.lb90@gmail.com>
2023-04-13 12:15:57 +0200

    premiere version refactorisation du code. Insertion de fonction pour générer les dots. code à revoir et bien relire

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,7 @@ const slides = [
 	}
 ]
 
+
 /**
  * raccrocher les éléments HTML au script
  */
@@ -27,90 +28,55 @@ const slideTagLine = document.querySelector("#banner > p");
  * déclaration et initialisation de l'indice du slide à zero
  */
 let slideIndice = 0; 
-let dotIndice = 0;
+let dotIndice = 0; 
 
 
 const dots = document.querySelector(".dots");
-const dot1 = dots.children.item(0);
-const dot2 = dots.children.item(1);
-const dot3= dots.children.item(2);
-const dot4 = dots.children.item(3);
+const dotsArray = [dots.children.item(0), dots.children.item(1), dots.children.item(2), dots.children.item(3)];
 
+function updateDot(){
+	dotsArray.forEach(function(dot,slideIndice){
+		if(slideIndice === dotIndice){
+			dot.classList.add("dot_selected");
+		}
+		else{
+			dot.classList.remove("dot_selected");
+		}
+	});
+}
 
 
 
 document.querySelector(".arrow_left").addEventListener("click", function(){
+	dotIndice -- ;
 	if (slideIndice <= 0){
 		slideIndice = slides.length;	
-	};
-	console.log(slideIndice);
-	switch(slideIndice){
-		case 4:
-			dot1.classList.remove("dot_selected");
-			dot2.classList.remove("dot_selected");
-			dot3.classList.remove("dot_selected");
-			dot4.classList.add("dot_selected");
-		break;
-		case 3:
-			dot1.classList.remove("dot_selected");
-			dot2.classList.remove("dot_selected");
-			dot3.classList.add("dot_selected");
-			dot4.classList.remove("dot_selected");
-		break;	
-		case 2:
-			dot3.classList.remove("dot_selected");
-			dot2.classList.add("dot_selected");
-			dot1.classList.remove("dot_selected");
-			dot4.classList.remove("dot_selected");	
-		break;	
-		case 1:
-			dot4.classList.remove("dot_selected");	
-			dot2.classList.remove("dot_selected");
-			dot3.classList.remove("dot_selected");
-			dot1.classList.add("dot_selected");
-		break;	
 	}
+	if(dotIndice< 0){
+		dotIndice = dotsArray.length-1;
+	}
+
+	updateDot();
 	slideImage.src = "./assets/images/slideshow/" + slides[slideIndice -1].image;
 	slideTagLine.innerHTML = slides[slideIndice -1].tagLine;
-	slideIndice--;		
+	slideIndice--;
+			
 } );
 
-
 document.querySelector(".arrow_right").addEventListener("click", function(){
+	dotIndice++;
 	if (slideIndice >= slides.length-1){
 		slideIndice = -1;
-	};
-	switch(slideIndice){
-		case -1:
-			dot1.classList.add("dot_selected");
-			dot2.classList.remove("dot_selected");
-			dot3.classList.remove("dot_selected");
-			dot4.classList.remove("dot_selected");
-		break;
-		case 0:
-			dot1.classList.remove("dot_selected");
-			dot2.classList.add("dot_selected");
-			dot3.classList.remove("dot_selected");
-			dot4.classList.remove("dot_selected");
-		break;	
-		case 1:
-			dot3.classList.add("dot_selected");
-			dot2.classList.remove("dot_selected");
-			dot1.classList.remove("dot_selected");
-			dot4.classList.remove("dot_selected");	
-		break;	
-		case 2:
-			dot4.classList.add("dot_selected");	
-			dot2.classList.remove("dot_selected");
-			dot3.classList.remove("dot_selected");
-			dot1.classList.remove("dot_selected");
-		break;	
+	}
+	if(dotIndice >= dotsArray.length){
+		dotIndice = 0;
 	}
 	slideImage.src = "./assets/images/slideshow/" + slides[slideIndice +1].image;
 	slideTagLine.innerHTML = slides[slideIndice +1].tagLine;
+	updateDot();
 	slideIndice++;
-	console.log(slideIndice);
-})
+	
+});
 
 
 
