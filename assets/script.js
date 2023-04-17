90235ccf7e5233df0ceec1c801c6f4528463f7d0
long bin <david.lb90@gmail.com>
2023-04-17 09:21:05 +0200

    modification du queryselector en getElementById pour selectionner l'image du slide

--- assets/script.js
+++ assets/script.js
@@ -18,12 +18,17 @@ const slides = [
   },
 ];
 
-const slideImage = document.querySelector(" #banner-slide");
+// deux constantes pour se raccrocher aux élements DOM
+const slideImage = document.getElementById("banner-slide");
 const slideTagLine = document.querySelector("#banner > p");
-let slideIndice = 0;
 
+// constante pour se raccrocher à tout les elements Dots
 const dots = document.querySelectorAll(".dot");
 
+// Initialisation de l'indice des slides à 0
+let slideIndice = 0;
+
+// fonction Pour générer les dots en fonction du slide indice. un foreach pour parcourir la liste des élements dots. cette fonction prends un parametre i. Parametre qui me permettrait d'ajuster la fonction en fonction de si on slidait à gauche ou si on slidait à droite.
 function updateDot(i) {
   dots.forEach(function (dot, index) {
     if (index === slideIndice + i) {
@@ -34,11 +39,13 @@ function updateDot(i) {
   });
 }
 
+// fonction pour generer les slides. Cette fonction prends un parametre i. Parametre qui me permettrait d'ajuster la fonction en fonction de si on slidait à gauche ou si on slidait à droite.
 function updateSlide(i) {
   slideImage.src = "./assets/images/slideshow/" + slides[slideIndice + i].image;
   slideTagLine.innerHTML = slides[slideIndice + i].tagLine;
 }
 
+// fonction qui gère l'ensemble des operations losque l'utilisateur clique sur la fleche de gauche ou sur la fleche de droite. En fonction de la direction, elle fait appelle aux fonctions crées plus haut: updateDot() et updateSlide(). En fonction de la direction cliquée, les fonctions updateDots() et updateSlide() prennent des parametres i différents.
 function changeDirection(direction) {
   if (direction === "left") {
     if (slideIndice <= 0) {
@@ -56,11 +63,11 @@ function changeDirection(direction) {
     updateSlide(0);
   }
 }
-// eventlistner pour la gauche
+// eventlistner pour la gauche qui fait appelle à la fonction changeDirection et en lui injectant le parametre "left"
 document.querySelector(".arrow_left").addEventListener("click", function () {
   changeDirection("left");
 });
-// eventlistner pour la droite
+// eventlistner pour la droite qui fait appelle à la fonction changeDirection et en lui injectant un parametre autre que 'left'
 document.querySelector(".arrow_right").addEventListener("click", function () {
   changeDirection("right");
 });
